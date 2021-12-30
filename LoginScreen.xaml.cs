8a8e6c2bb07855c029ccf4b2c6b84f2c7237492f
Andy Anggara <andyanggara96@gmail.com>
2021-12-30 18:41:47 +0700

    Add files via upload

---
+++ LoginScreen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UUJ05RB; Initial Catalog=db0; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tbl_User WHERE Username=@Username AND Password=@Password";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if (count == 1)
+                {
+                    MainWindow dashboard = new MainWindow();
+                    dashboard.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username/password yang anda masukkan salah. Silahkan coba lagi.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlCon = new SqlConnection(@"Data Source=DESKTOP-UUJ05RB; Initial Catalog=db0; Integrated Security=True;");
+        SqlCommand cmd;
+        SqlDataAdapter adpt;
+        DataTable dt;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void summonLab(String lab)
+        {
+            adpt = new SqlDataAdapter("SELECT id_Item, nama_item, spec_Item, ketersediaan FROM dbo.tbl_Item WHERE nama_Lab =  '" + lab + "'", sqlCon);
+            dt = new DataTable();
+            adpt.Fill(dt);
+            dt.Columns["id_item"].ColumnName = "ID";
+            dt.Columns["nama_item"].ColumnName = "Nama Barang";
+            dt.Columns["spec_item"].ColumnName = "Spesifikasi";
+            dt.Columns["Ketersediaan"].ColumnName = "Ketersediaan";
+            DataGridView.ItemsSource = dt.DefaultView;
+        }
+
+        private void labMesin_Click(object sender, RoutedEventArgs e)
+        {
+            summonLab("lab_mesin");
+        }
+
+        private void labListrik_Click(object sender, RoutedEventArgs e)
+        {
+            summonLab("lab_listrik");
+        }
+
+        private void labPangan_Click(object sender, RoutedEventArgs e)
+        {
+            summonLab("lab_pangan");
+        }
+
+        private void labTanah_Click(object sender, RoutedEventArgs e)
+        {
+            summonLab("lab_tanah");
+        }
+
+        private void labPasca_Click(object sender, RoutedEventArgs e)
+        {
+            summonLab("lab_pasca");
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonLogout_Click(object sender, RoutedEventArgs e)
+        {            
+            LoginScreen login = new LoginScreen();
+            login.Show();
+            this.Close();
+        }
+    }
+}
